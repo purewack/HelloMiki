a1180301ce702a00f56bb80af3301eb21e475ae9
Damian Nowacki <bobimaster15@gmail.com>
2023-08-23 15:27:22 +0100

    [main] env setup and test hardware

--- cat_mat_detector.ino
+++ cat_mat_detector.ino
@@ -1,9 +1,11 @@
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println("hello wardware");
+  delay(1000);
 }
