63a0cb158aebe6c4144ad7462ed56650f0069cb4
purewack <bobimaster15@gmail.com>
2023-12-15 00:20:53 +0000

    [dev] automatic dark mode with override

--- app/internal/src/Helpers.js
+++ app/internal/src/Helpers.js
@@ -65,3 +65,35 @@ export function motion(event, last = undefined, setter, atTime = undefined){
 export function arm(event, last = undefined, setter, atTime = undefined){
   setEventAuto('arm',last,{event},setter, atTime)
 }
+
+export function isDaytime(timestamp) {
+  const date = new Date(timestamp);
+
+  // Define sunrise and sunset times for each month (in minutes from midnight)
+  const monthlyTimes = [
+    { sunrise: 490, sunset: 1080 }, // January
+    { sunrise: 430, sunset: 1100 }, // February
+    { sunrise: 390, sunset: 1120 }, // March
+    { sunrise: 350, sunset: 1140 }, // April
+    { sunrise: 330, sunset: 1260 }, // May
+    { sunrise: 300, sunset: 1280 }, // June
+    { sunrise: 300, sunset: 1280 }, // July
+    { sunrise: 330, sunset: 1260 }, // August
+    { sunrise: 350, sunset: 1140 }, // September
+    { sunrise: 390, sunset: 1120 }, // October
+    { sunrise: 430, sunset: 1100 }, // November
+    { sunrise: 490, sunset: 1080 }, // December
+  ];
+
+  // Get the corresponding times for the current month
+  const currentMonth = date.getMonth();
+  const currentSunrise = monthlyTimes[currentMonth].sunrise;
+  const currentSunset = monthlyTimes[currentMonth].sunset;
+
+  // Calculate the total minutes since midnight
+  const minutesSinceMidnight = date.getHours() * 60 + date.getMinutes();
+  const isDay = (minutesSinceMidnight >= currentSunrise) && (minutesSinceMidnight <= currentSunset);
+  // Check if it's currently within the sunrise and sunset times
+  console.log(timestamp,isDay, minutesSinceMidnight, currentSunrise, currentSunset)
+  return isDay;
+}
